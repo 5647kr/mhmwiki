e233bcf73eae4ab712bca9bd4b7bb2406547ebc3
5647kr <5647kr@gmail.com>
2026-01-23 21:54:44 +0900

    제보 코드 수정

--- js/report.js
+++ js/report.js
@@ -6,10 +6,12 @@ const reportForm = reportWrap.querySelector("form");
 
 reportBtn.addEventListener("click", () => {
   reportWrap.classList.add("active");
+  document.body.style.overflow = "hidden";
 });
 
 closeBtn.addEventListener("click", () => {
   reportWrap.classList.remove("active");
+  document.body.style.overflow = "visible";
 });
 
 const part1 = "https://discord.com/api/webhooks/1464231926566092887/";

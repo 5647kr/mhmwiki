3ee79e4e751e18690cd586c83c3c7594fcb6aca8
5647kr <5647kr@gmail.com>
2026-06-17 13:17:05 +0900

    settings/#1/ 개발 환경 구축 및 기본 초기 세팅

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Default from "./routes/layouts/Default";
+import Home from "./routes/pages/Home";
+import Detail from "./routes/pages/Detail";
+import NotFound from "./routes/pages/NotFound";
+import History from "./routes/pages/History";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    Component: Default,
+    children: [
+      {
+        path: "/",
+        Component: Home,
+      },
+      { path: "/:id", Component: Detail },
+      { path: "/history", Component: History },
+      { path: "*", Component: NotFound },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
